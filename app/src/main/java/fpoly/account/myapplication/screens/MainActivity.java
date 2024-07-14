4d752798f2fe684ed30c4533e5ca00df808a2dd3
newbieofcoder <cauvang13102001@gmail.com>
2024-07-14 08:42:25 +0700

    if

--- app/src/main/java/fpoly/account/myapplication/screens/MainActivity.java
+++ app/src/main/java/fpoly/account/myapplication/screens/MainActivity.java
@@ -1,6 +1,9 @@
 package fpoly.account.myapplication.screens;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -34,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     private Button add;
     private ToDoDAO toDoDAO;
     private ToDo currentToDo = null;
+    private Context mContext = this;
     AlertDialog.Builder builder;
 
     @Override
@@ -52,26 +56,36 @@ public class MainActivity extends AppCompatActivity {
         edtdate = findViewById(R.id.tv_date);
         edttype = findViewById(R.id.tv_type);
         add = findViewById(R.id.btnAdd);
-        toDoDAO = new ToDoDAO(this);
+        toDoDAO = new ToDoDAO(mContext);
         list = toDoDAO.getAllData();
         adapter = new ToDoAdapter(list);
         SpacingItemDecoration spacingItemDecoration = new SpacingItemDecoration(20);
         recyclerView.addItemDecoration(spacingItemDecoration);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(adapter);
+        edttype.setOnClickListener(v -> {
+            String[] type = {"De", "Trung binh", "Kho"};
+            AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+            builder.setTitle("Chon muc do kho cua cong viec");
+            builder.setItems(type, (dialog, which) -> {
+                edttype.setText(type[which]);
+            });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        });
         add.setOnClickListener(v -> {
             String title = edttitle.getText().toString();
             String content = edtcontent.getText().toString();
             String date = edtdate.getText().toString();
             String type = edttype.getText().toString();
             if (title.isEmpty() || content.isEmpty() || date.isEmpty() || type.isEmpty()) {
-                Toast.makeText(this, "Nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+                Toast.makeText(mContext, "Nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
             } else {
                 ToDo toDo = new ToDo(0, title, content, date, type, 0);
                 toDoDAO.addToDo(toDo);
                 list = toDoDAO.getAllData();
                 adapter.notifyDataSetChanged();
-                Toast.makeText(this, "Thêm thành công", Toast.LENGTH_SHORT).show();
+                Toast.makeText(mContext, "Thêm thành công", Toast.LENGTH_SHORT).show();
                 edttitle.setText("");
                 edtcontent.setText("");
                 edtdate.setText("");
@@ -91,8 +105,9 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onDeleteClick(int position) {
-                builder = new AlertDialog.Builder(MainActivity.this);
+                builder = new AlertDialog.Builder(mContext);
                 builder.setTitle("Xác nhận xoá");
+                builder.setIcon(R.drawable.baseline_warning_24);
                 builder.setMessage("Bạn có chắc chắn muốn xóa?");
                 builder.setPositiveButton("Có", (dialog, which) -> {
                     ToDo toDo = list.get(position);
@@ -113,34 +128,52 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onEditClick(int position) {
                 ToDo toDo = list.get(position);
-                String title = edttitle.getText().toString();
-                String content = edtcontent.getText().toString();
-                String date = edtdate.getText().toString();
-                String type = edttype.getText().toString();
-                if (title.isEmpty() || content.isEmpty() || date.isEmpty() || type.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
-                } else {
-                    builder = new AlertDialog.Builder(MainActivity.this);
-                    builder.setTitle("Xác nhận sửa");
-                    builder.setMessage("Bạn có chắc chắn muốn sửa dòng " + String.valueOf(position + 1) +" ?");
-                    builder.setPositiveButton("Có", (dialog, which) -> {
+                View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.custom_edit_dialog, null);
+                EditText edttitle = view.findViewById(R.id.tv_edit_title);
+                EditText edtcontent = view.findViewById(R.id.tv_edit_content);
+                EditText edtdate = view.findViewById(R.id.tv_edit_date);
+                EditText edttype = view.findViewById(R.id.tv_edit_type);
+                edttitle.setText(toDo.getTitle());
+                edtcontent.setText(toDo.getContent());
+                edtdate.setText(toDo.getDate());
+                edttype.setText(toDo.getType());
+                edttype.setOnClickListener(v -> {
+                    String[] type = {"De", "Trung binh", "Kho"};
+                    AlertDialog.Builder builder1 = new AlertDialog.Builder(mContext);
+                    builder1.setTitle("Chon muc do kho cua cong viec");
+                    builder1.setItems(type, (dialog, which) -> {
+                        edttype.setText(type[which]);
+                    });
+                    AlertDialog alertDialog = builder1.create();
+                    alertDialog.show();
+                });
+                Button btnUpdate = view.findViewById(R.id.btnUpdate);
+                Button btnCancel = view.findViewById(R.id.btnCancel);
+                AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
+                builder.setView(view);
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+                btnUpdate.setOnClickListener(v -> {
+                    String title = edttitle.getText().toString();
+                    String content = edtcontent.getText().toString();
+                    String date = edtdate.getText().toString();
+                    String type = edttype.getText().toString();
+                    if (title.isEmpty() || content.isEmpty() || date.isEmpty() || type.isEmpty()) {
+                        Toast.makeText(MainActivity.this, "Nhập đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+                    } else {
                         toDo.setTitle(title);
                         toDo.setContent(content);
                         toDo.setDate(date);
                         toDo.setType(type);
                         toDoDAO.updateToDo(toDo);
-                        adapter.notifyDataSetChanged();
-                        Toast.makeText(MainActivity.this, "Sửa thành công", Toast.LENGTH_SHORT).show();
-                        edttitle.setText("");
-                        edtcontent.setText("");
-                        edtdate.setText("");
-                        edttype.setText("");
                         list = toDoDAO.getAllData();
+                        adapter.notifyDataSetChanged();
                         onResume();
-                    });
-                    builder.setNegativeButton("Không", null);
-                    builder.show();
-                }
+                        Toast.makeText(MainActivity.this, "Cập nhật thành công", Toast.LENGTH_SHORT).show();
+                        alertDialog.dismiss();
+                    }
+                });
+                btnCancel.setOnClickListener(v -> alertDialog.dismiss());
             }
 
             @Override
@@ -148,9 +181,9 @@ public class MainActivity extends AppCompatActivity {
                 ToDo toDo = list.get(position);
                 toDo.setStatus(isDone ? 1 : 0);
                 toDoDAO.updateStatusToDo(toDo);
-                adapter.notifyDataSetChanged();
                 list = toDoDAO.getAllData();
                 onResume();
+                adapter.notifyDataSetChanged();
             }
         });
     }
@@ -159,7 +192,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         adapter = new ToDoAdapter(list);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new LinearLayoutManager(mContext));
         recyclerView.setAdapter(adapter);
     }
 }
